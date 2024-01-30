8e0d78e6dd741fc3f3396b6c0201f67aa9e542fc
Blessing Ogor <ogorblessing96@gmail.com>
2024-01-30 21:27:51 +0100

    print perctg

--- format_char.c
+++ format_char.c
@@ -4,15 +4,16 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-/*
- * print_char - print a charactacter to std out
+/**
+ * print_char - print a character to std out
  * @c: a character to be printed
  * Return: on success return 1
  */
+
 int print_char(va_list c)
 {
 	char txt;
-	
+
 	txt = va_arg(c, int);
 
 	_putchar(txt);
@@ -22,9 +23,9 @@ int print_char(va_list c)
 
 /*
  * print_perctg - prints a character % on std out
- *
- * Return: 1 (success)
+ * Return: 1
  */
+
 int print_perctg()
 {
 	_putchar('%');
